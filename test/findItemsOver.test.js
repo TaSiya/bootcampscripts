e44a35cb2ab4f360d76a9744353f8652d7e9e7b1
TaSiya <siyandamakhalima.1@gmail.com>
2018-03-20 10:44:37 +0200

    finished find items over 20 and just added function find items over

---
+++ test/findItemsOver.test.js
@@ -0,0 +1,24 @@
+
+let itemList = [
+{name : 'apples', qty : 10},
+{name : 'pears', qty : 37},
+{name : 'bananas', qty : 27},
+{name : 'apples', qty : 3},
+];
+
+let results = [
+{name : 'pears', qty : 37},
+{name : 'apples', qty : 10},
+{name : 'bananas', qty : 27},
+];
+
+describe('Find the items over given threshold ', function(){
+
+   it('checks if the given list objects are over given threshold ', function(){
+      assert.deepEqual(findItemsOver20(itemList,5),results);
+   });
+
+   it('checks if the output is an Array', function(){
+      assert.isArray(findItemsOver20(itemList,2), 'is a array');
+   });
+});
--- test/findItemsOver20.test.js
+++ test/findItemsOver20.test.js
@@ -1,14 +1,23 @@
 
-describe('Find the items over 20', function(){
-
-   let itemList = [
-    {name : 'apples', qty : 10},
-    {name : 'pears', qty : 37},
-    {name : 'bananas', qty : 27},
-    {name : 'apples', qty : 3},
+let itemList = [
+{name : 'apples', qty : 10},
+{name : 'pears', qty : 37},
+{name : 'bananas', qty : 27},
+{name : 'apples', qty : 3},
 ];
 
+let results = [
+{name : 'pears', qty : 37},
+{name : 'bananas', qty : 27},
+];
+
+describe('Find the items over 20', function(){
+
    it('checks if the given list objects are over 20 ', function(){
-      assert.equal(findItemsOver20(itemList),[]{"name":"pears","qty":37},{"name":"bananas","qty":27}]);
+      assert.deepEqual(findItemsOver20(itemList),results);
+   });
+
+   it('checks if the output is an Array', function(){
+      assert.isArray(findItemsOver20(itemList), 'is a object');
    });
 });
