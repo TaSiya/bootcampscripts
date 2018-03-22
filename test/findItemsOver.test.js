6dd2cd3a53af28f0a7ebf0377c7f2ad337ff51bb
TaSiya <siyandamakhalima.1@gmail.com>
2018-03-22 14:09:18 +0200

    finished will the testing tasks but only have 1 test that is no passing in the function findItemsOver

--- test/findItemsOver.test.js
+++ test/findItemsOver.test.js
@@ -7,15 +7,16 @@ let itemList2 = [
 {name : 'apples', qty : 3},
 ];
 
-let results1 = [
-{name : 'pears', qty : 37},
-{name : 'apples', qty : 10},
-{name : 'orange', qty : 5},
-{name : 'bananas', qty : 27},
-{name : 'apples', qty : 3},
-];
 
 describe('Find the items over given threshold', function(){
+
+   let results1 = [
+   {name : 'pears', qty : 37},
+   {name : 'apples', qty : 10},
+   {name : 'orange', qty : 5},
+   {name : 'bananas', qty : 27},
+   {name : 'apples', qty : 3},
+];
 
    it('checks if the given list of objects are over given threshold ', function(){
       assert.deepEqual(findItemsOver(itemList2,0),results1);
