dd5ac52fb947c1aa7f87cc7c0f59fa0b1c5e80b8
TaSiya <siyandamakhalima.1@gmail.com>
2018-03-23 16:36:32 +0200

    final commit i promise oyu

--- test/greet.test.js
+++ test/greet.test.js
@@ -6,6 +6,16 @@ describe('greeting function', function(){
       assert.equal(greet('siya'), 'Hello, siya');
    });
 
+   it('says hello to John.', function(){
+
+      assert.equal(greet('John'), 'Hello, John');
+   });
+
+   it('says hello to everyone.', function(){
+
+      assert.equal(greet('everyone'), 'Hello, everyone');
+   });
+
    it('Check the type', function(){
       assert.typeOf(greet('John'), 'string');
    });
--- test/mostProfitableDepartment.test.js
+++ test/mostProfitableDepartment.test.js
@@ -1,18 +1,32 @@
 
-let salesDate =[
-   {department : 'hardware', sales : 4500},
-   {department : 'outdoor', sales : 1500},
-   {department : 'outdoor', sales : 500},
-   {department : 'electronics', sales : 5500},
-   {department : 'hardware', sales : 3500},
-   {department : 'outdoor', sales : 2000},
-   {department : 'hardware', sales : 4500},
-];
+
 
 describe('Most Profitable Department', function(){
 
+   let salesDate =[
+      {department : 'hardware', sales : 4500},
+      {department : 'outdoor', sales : 1500},
+      {department : 'outdoor', sales : 500},
+      {department : 'electronics', sales : 5500},
+      {department : 'hardware', sales : 3500},
+      {department : 'outdoor', sales : 2000},
+      {department : 'hardware', sales : 4500},
+   ];
+
+   let salesDate2 =[
+      {department : 'outdoor', sales : 1500},
+      {department : 'outdoor', sales : 500},
+      {department : 'electronics', sales : 10000},
+      {department : 'hardware', sales : 3500},
+      {department : 'outdoor', sales : 2000},
+   ];
+
    it('checks profitable department', function(){
       assert.deepEqual(mostProfitableDepartment(salesDate),'hardware')
+   });
+
+   it('checks profitable department', function(){
+      assert.deepEqual(mostProfitableDepartment(salesDate2),'electronics')
    });
 
    it('check type', function(){
